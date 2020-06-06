fbb32f4e963d84b1961f14a438de5267ccdba512
AleksiSalminen <aleksisalminen1234@gmail.com>
2020-06-06 12:07:34 +0300

    Start for the slides table added

--- app/src/SlideLinker.js
+++ app/src/SlideLinker.js
@@ -4,12 +4,53 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
 
 function SlidesTable(props) {
 
+  const [slidesPerPage, setSlidesPerPage] = React.useState(10);
+
+  let slides = [{"image": "(image)", "heading": "(heading)", "description": "(description)"}, {"image": "(image)", "heading": "(heading)", "description": "(description)"}];
+
   return (
     <div>
+    
+    <Paper>
+      <TableContainer>
+        <Table stickyHeader aria-label="slides table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Add</TableCell>
+							<TableCell>Heading</TableCell>
+              <TableCell>Description</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{slides.map((row, index) => (
+							<TableRow key={row.name + "-" + index}>
+								<TableCell component="th" scope="row">
+									{row.image} 
+								</TableCell>
+								<TableCell>
+									{row.heading}
+								</TableCell>
+                <TableCell>
+									{row.description}
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+      </TableContainer>
+    </Paper>
 
     </div>
   );
