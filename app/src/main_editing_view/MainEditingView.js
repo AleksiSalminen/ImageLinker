f982baf2d6df5553a400e8e442607c581618d78d
AleksiSalminen <aleksisalminen1234@gmail.com>
2020-06-06 16:49:01 +0300

    Improvements to open project dialog

--- app/src/main_editing_view/MainEditingView.js
+++ app/src/main_editing_view/MainEditingView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -16,27 +16,44 @@ import SlidesTable from './SlidesTable.js';
 function MainEditingView(props) {
     const selectedProject = props.selectedProjectInfo.selected
     const projectName = (selectedProject ? selectedProject.name : "(Create or open a project)");
+    const [projectDialogOpen, setProjectDialogOpen] = useState(false);
+    const [projects, setProjects] = useState([]);
 
-    
+    const openProjectDialog = () => {
+        setProjects([{"id":"1", "name":"First"}, {"id":"2", "name":"Second"}]);
+        setProjectDialogOpen(true);
+    }
+
+    const changeSelectedProject = (id) => {
+        if(id) {
+            alert(id);
+        }
+
+        setProjectDialogOpen(false);
+    }
+
     return (
         <div>
 
         <AppBar position="static">
-        <Toolbar>
-            <Box>
-            <Button variant="contained" color="primary">New</Button>
-            <OpenProjectDialog />
-            <Button variant="contained" color="primary">Play</Button>
-            <Button variant="contained" color="primary">Settings</Button>
-            </Box>
-        </Toolbar>
+            <Toolbar>
+                <Box>
+                    <Button variant="contained" color="primary">New</Button>
+                    <Button variant="contained" color="primary" onClick={openProjectDialog}>Open</Button>
+                    <Button variant="contained" color="primary">Play</Button>
+                    <Button variant="contained" color="primary">Settings</Button>
+                </Box>
+            </Toolbar>
         </AppBar>
+
+        <OpenProjectDialog open={projectDialogOpen} projects={projects} changeSelectedProject={changeSelectedProject}/>
+
         <br/>
         <Box>
-        <Typography variant="h4" align="center">
-            {projectName}&nbsp;
-            <Button variant="contained">Change</Button>
-        </Typography>
+            <Typography variant="h4" align="center">
+                {projectName}&nbsp;
+                <Button variant="contained">Change</Button>
+            </Typography>
         </Box>
         <br/><br/>
         <SlidesTable/>
--- app/src/main_editing_view/OpenProjectDialog.js
+++ app/src/main_editing_view/OpenProjectDialog.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,52 +6,29 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 
 
-const projects = ['project1', 'project2'];
-
-function SimpleDialog(props) {
-    const { onClose, selectedValue, open } = props;
-
-    const handleClose = () => {
-        onClose(selectedValue);
-    };
-
-    const handleListItemClick = (value) => {
-        onClose(value);
-    };
-
-    return (
-        <Dialog onClose={handleClose} aria-labelledby="Open project dialog" open={open}>
-            <DialogTitle id="open-project-title">Open project</DialogTitle>
-            <List>
-                {projects.map((project) => (
-                    <ListItem button onClick={() => handleListItemClick(project)} key={project}>
-                        <ListItemText primary={project} />
-                    </ListItem>
-                ))}
-            </List>
-        </Dialog>
-    );
-}
-
-function OpenProjectDialog() {
-    const [open, setOpen] = React.useState(false);
-    const [selectedValue, setSelectedValue] = React.useState(projects[1]);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = (value) => {
-        setOpen(false);
-        setSelectedValue(value);
-    };
-
-    return (
-        <div>
-            <Button variant="contained" color="primary" onClick={handleClickOpen}>Open</Button>
-            <SimpleDialog selectedValue={selectedValue} open={open} onClose={handleClose} />
-        </div>
-    );
+/**
+ * Function that returns the dialog for choosing and opening a project
+ * @param {Object} props received parameters
+ */
+function OpenProjectDialog(props) {
+    
+    if(props.open) {
+        return (
+            <Dialog onClose={() => {props.changeSelectedProject(null)}} aria-labelledby="Open project dialog" open={true}>
+                <DialogTitle id="open-project-title">Open project</DialogTitle>
+                <List>
+                    {props.projects.map((project) => (
+                        <ListItem button onClick={() => props.changeSelectedProject(project.id)} key={project.id}>
+                            <ListItemText primary={project.name} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Dialog>
+        );
+    }
+    else {
+        return (<div></div>);
+    }
 }
 
 export default OpenProjectDialog;
