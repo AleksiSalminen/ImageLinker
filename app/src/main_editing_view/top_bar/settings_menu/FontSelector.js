2b8576490c76bfc899097c40e0294b2348340eb8
AleksiSalminen <aleksisalminen1234@gmail.com>
2020-06-09 16:33:58 +0300

    Font selecting working

--- app/src/main_editing_view/top_bar/settings_menu/FontSelector.js
+++ app/src/main_editing_view/top_bar/settings_menu/FontSelector.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import fonts from './fonts.js';
 
@@ -14,7 +16,19 @@ function FontSelector(props) {
 
     return (
         <MenuItem>
-            A
+            {t("TopBar.Settings.Font")}:
+            &nbsp;&nbsp;
+
+            <Autocomplete
+                id="font-select"
+                value={props.font}
+                onChange={(event, newFont) => {props.setFont(newFont)}}
+                options={fonts}
+                disableClearable={true}
+                getOptionLabel={(option) => option}
+                style={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} variant="outlined" />}
+            />
         </MenuItem>
     );
 }
