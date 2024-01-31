ed37c677fb4adb786a9d46bc04b07771a8d3a1d9
Aleksi Salminen <aleksisalminen1234@gmail.com>
2024-01-31 09:34:55 +0200

    FRONTEND: Start for user register frontend functionality

---
+++ frontend/src/src/main_editing_view/top_bar/profile_menu/RegisterButton.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
+import Button from "@mui/material/Button"
+
+import RegisterDialog from "./RegisterDialog.jsx"
+
+/**
+ * Function that returns the button for switching to register view
+ * @param {Object} props received parameters
+ */
+function RegisterButton(props) {
+    const { t } = useTranslation()
+
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+    const openRegisterDialog = () => {
+        setDialogOpen(true)
+    }
+
+    const closeRegisterDialog = () => {
+        setDialogOpen(false)
+    }
+
+    return (
+        <>
+            <Button
+                variant="contained"
+                color="primary"
+                style={{ margin: "0.25rem" }}
+                onClick={openRegisterDialog}
+            >
+                {t("TopBar.RegisterButton")}
+            </Button>
+
+            <RegisterDialog
+                open={dialogOpen}
+                closeDialog={closeRegisterDialog}
+                fetchUserRegister={props.fetchUserRegister}
+            />
+        </>
+    )
+}
+
+export default RegisterButton
---
+++ frontend/src/src/main_editing_view/top_bar/profile_menu/RegisterDialog.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
+import Box from "@mui/material/Box"
+import Dialog from "@mui/material/Dialog"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import TextField from "@mui/material/TextField"
+
+/**
+ * Function that returns the option editing dialog
+ * @param {Object} props received parameters
+ */
+function RegisterDialog(props) {
+    const { t } = useTranslation()
+
+    const [userName, setUserName] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    const updateUserName = (event) => {
+        setUserName(event.target.value)
+    }
+
+    const updatePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const register = () => {
+        const user = {
+            userName: userName,
+            password: password,
+        }
+
+        props.fetchUserRegister(user).then((error) => {
+            if (error) {
+                const alertMsg = t("TopBar.RegisterFailed")
+                alert(alertMsg)
+            }
+        })
+    }
+
+    return (
+        <Dialog open={props.open} style={{ padding: "0.5rem" }}>
+            <Box style={{ padding: "1rem" }}>
+                <Typography variant="h4">
+                    {t("TopBar.RegisterTitle")}
+                </Typography>
+
+                <TextField
+                    label={t("TopBar.RegisterUserName")}
+                    value={userName}
+                    type="text"
+                    onChange={updateUserName}
+                />
+                <br />
+
+                <TextField
+                    label={t("TopBar.RegisterPassword")}
+                    value={password}
+                    type="password"
+                    onChange={updatePassword}
+                />
+
+                <Box style={{ marginTop: "1rem" }}>
+                    <Button
+                        onClick={register}
+                        variant="contained"
+                        color="primary"
+                        style={{ marginRight: "2rem" }}
+                    >
+                        {t("TopBar.RegisterButton")}
+                    </Button>
+
+                    <Button
+                        onClick={props.closeDialog}
+                        variant="contained"
+                        color="default"
+                        style={{ marginRight: "2rem" }}
+                    >
+                        {t("TopBar.CancelButton")}
+                    </Button>
+                </Box>
+            </Box>
+        </Dialog>
+    )
+}
+
+export default RegisterDialog
