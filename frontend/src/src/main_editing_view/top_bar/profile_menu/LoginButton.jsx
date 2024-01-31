d69aeb1a27e3eba282399bf543abe091af43abd6
Aleksi Salminen <aleksisalminen1234@gmail.com>
2024-01-31 09:35:43 +0200

    FRONTEND: Start for user login frontend functionality

---
+++ frontend/src/src/main_editing_view/top_bar/profile_menu/LoginButton.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
+import Button from "@mui/material/Button"
+
+import LoginDialog from "./LoginDialog.jsx"
+
+/**
+ * Function that returns the button for switching to login view
+ * @param {Object} props received parameters
+ */
+function LoginButton(props) {
+    const { t } = useTranslation()
+
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+    const openLoginDialog = () => {
+        setDialogOpen(true)
+    }
+
+    const closeLoginDialog = () => {
+        setDialogOpen(false)
+    }
+
+    return (
+        <>
+            <Button
+                variant="contained"
+                color="primary"
+                style={{ margin: "0.25rem" }}
+                onClick={openLoginDialog}
+            >
+                {t("TopBar.LoginButton")}
+            </Button>
+
+            <LoginDialog
+                open={dialogOpen}
+                closeDialog={closeLoginDialog}
+                fetchUserLogin={props.fetchUserLogin}
+            />
+        </>
+    )
+}
+
+export default LoginButton
---
+++ frontend/src/src/main_editing_view/top_bar/profile_menu/LoginDialog.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
+import Box from "@mui/material/Box"
+import Dialog from "@mui/material/Dialog"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import TextField from "@mui/material/TextField"
+
+/**
+ * Function that returns the option editing dialog
+ * @param {Object} props received parameters
+ */
+function LoginDialog(props) {
+    const { t } = useTranslation()
+
+    const [userName, setUserName] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    const updateUserName = (event) => {
+        setUserName(event.target.value)
+    }
+
+    const updatePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const login = () => {
+        const user = {
+            userName: userName,
+            password: password,
+        }
+
+        props.fetchUserLogin(user).then((error) => {
+            if (error) {
+                const alertMsg = t("TopBar.LoginFailed")
+                alert(alertMsg)
+            }
+        })
+    }
+
+    return (
+        <Dialog open={props.open} style={{ padding: "0.5rem" }}>
+            <Box style={{ padding: "1rem" }}>
+                <Typography variant="h4">{t("TopBar.LoginTitle")}</Typography>
+
+                <TextField
+                    label={t("TopBar.LoginUserName")}
+                    value={userName}
+                    type="text"
+                    onChange={updateUserName}
+                />
+                <br />
+
+                <TextField
+                    label={t("TopBar.LoginPassword")}
+                    value={password}
+                    type="password"
+                    onChange={updatePassword}
+                />
+
+                <Box style={{ marginTop: "1rem" }}>
+                    <Button
+                        onClick={login}
+                        variant="contained"
+                        color="primary"
+                        style={{ marginRight: "2rem" }}
+                    >
+                        {t("TopBar.LoginButton")}
+                    </Button>
+
+                    <Button
+                        onClick={props.closeDialog}
+                        variant="contained"
+                        color="default"
+                        style={{ marginRight: "2rem" }}
+                    >
+                        {t("TopBar.CancelButton")}
+                    </Button>
+                </Box>
+            </Box>
+        </Dialog>
+    )
+}
+
+export default LoginDialog
