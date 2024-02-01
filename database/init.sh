ceda7cfd8a63704e17abbaa59645418d8e301770
Aleksi Salminen <aleksisalminen1234@gmail.com>
2024-02-01 14:43:49 +0200

    DATABASE: I give up for now

--- database/init.sh
+++ database/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cat << EOF > /data/application/init.sql
 -- Create the database
-CREATE DATABASE '${MYSQL_DATABASE}';
+CREATE DATABASE ${MYSQL_DATABASE};
 
 -- Select the database to use
-USE '${MYSQL_DATABASE}';
+USE ${MYSQL_DATABASE};
 
 -- Add user table
 CREATE TABLE user(
@@ -16,7 +16,7 @@ CREATE TABLE user(
 );
 
 -- Create user
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
+CREATE USER ${MYSQL_USER}@% IDENTIFIED BY"${MYSQL_PASSWORD}";
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@%;
 FLUSH PRIVILEGES;
 EOF
