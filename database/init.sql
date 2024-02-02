5992f244648ce71d9045811880dfc1fbbf07b544
Aleksi Salminen <aleksisalminen1234@gmail.com>
2024-02-02 14:01:41 +0200

    DATABASE: Basic custom initialization (finally?) working

---
+++ database/init.sql
@@ -0,0 +1,15 @@
+
+-- Create the database
+CREATE DATABASE slidelinker;
+
+-- Select the database to use
+USE slidelinker;
+
+-- Add user table
+CREATE TABLE user(
+   id INT PRIMARY KEY,
+   name VARCHAR(100),
+   email VARCHAR(255),
+   user_name VARCHAR(50),
+   password CHAR(64)
+);
