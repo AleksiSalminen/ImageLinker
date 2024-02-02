9f3248edc82a59d11177e29c5a3f1c370fb74e60
Aleksi Salminen <aleksisalminen1234@gmail.com>
2024-02-02 14:42:52 +0200

    DATABASE: Added some test data

--- database/init.sql
+++ database/init.sql
@@ -1,3 +1,6 @@
+-- ----------------------- --
+-- DATABASE INITIALIZATION --
+-- ----------------------- --
 
 -- Create the database
 CREATE DATABASE slidelinker;
@@ -7,9 +10,19 @@ USE slidelinker;
 
 -- Add user table
 CREATE TABLE user(
-   id INT PRIMARY KEY,
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   user_name VARCHAR(50),
    name VARCHAR(100),
    email VARCHAR(255),
-   user_name VARCHAR(50),
-   password CHAR(64)
+   password CHAR(60)
 );
+
+-- ------------------------ --
+-- ------ TEST DATA ------- --
+-- ------------------------ --
+
+-- Table: user
+-- (Password: test)
+INSERT INTO user (user_name, name, email, password) VALUES ('test', 'Test User', 'user@test.com', '$2a$04$X/Wco1QSm74GpaQFfEff8.kACjPRaZPTKznLT5j3GXxltBQTE667S');
+INSERT INTO user (user_name, name, email, password) VALUES ('test2', 'Test2 User', 'user2@test.com', '$2a$04$X/Wco1QSm74GpaQFfEff8.kACjPRaZPTKznLT5j3GXxltBQTE667S');
+INSERT INTO user (user_name, name, email, password) VALUES ('test3', 'Test3 User', 'user3@test.com', '$2a$04$X/Wco1QSm74GpaQFfEff8.kACjPRaZPTKznLT5j3GXxltBQTE667S');
