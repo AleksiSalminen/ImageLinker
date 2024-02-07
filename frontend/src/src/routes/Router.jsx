76dda37fe1d3981aafdfa19cf95bfa371a431070
Aleksi Salminen <aleksisalminen1234@gmail.com>
2024-02-07 10:04:25 +0200

    FRONTEND: Basic React Router setup (buggy)

---
+++ frontend/src/src/routes/Router.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { HashRouter, Route, Routes } from "react-router-dom"
+
+import MainEditingView from "../main_editing_view/MainEditingView"
+import PresentationView from "../presentation_view/PresentationView"
+import SlideEditingView from "../slide_editing_view/SlideEditingView"
+
+const PATHS = {
+    mainEditingView: "/main",
+    presentationView: "/presentation",
+    slideEditingView: "/slide",
+}
+
+function Router() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route index element={<MainEditingView />} />
+                <Route path={PATHS.mainEditingView} element={<MainEditingView />} />
+                <Route path={PATHS.presentationView} element={<PresentationView />} />
+                <Route path={PATHS.slideEditingView} element={<SlideEditingView />} />
+            </Routes>
+        </HashRouter>
+    )
+}
+
+export default Router
